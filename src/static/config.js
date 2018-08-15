b3f2181848314f2d6f625763f1f586ed3eb68a06
piaoyidage <longyixiaoge@gmail.com>
2018-08-15 19:37:55 +0800

    add music

--- src/static/config.js
+++ src/static/config.js
@@ -2,21 +2,27 @@
 * @Author: maoying.hu
 * @Date:   2018-08-12 22:15:55
 * @Last Modified by:   maoying.hu
-* @Last Modified time: 2018-08-15 14:33:44
+* @Last Modified time: 2018-08-15 18:01:42
 */
 
 const MusicList = [
 	{
-		name: '魔鬼中的天使',
-		singer: '田馥甄',
-		url: require('./music/魔鬼中的天使（田馥甄）.mp3'),
-		duration: '03:58',
+		name: '唯一',
+		singer: '王力宏',
+		url: require('./music/唯一（王力宏）.mp3'),
+		duration: '04:22',
 	},
 	{
-		name: '上海滩',
-		singer: '叶丽仪',
-		url: require('./music/上海滩（叶丽仪）.mp3'),
-		duration: '03:13',
+		name: '值得一辈子去爱',
+		singer: '纪如景',
+		url: require('./music/值得一辈子去爱（纪如景）.mp3'),
+		duration: '03:47',
+	},
+	{
+		name: '你是我最深爱的人',
+		singer: '永邦',
+		url: require('./music/你是我最深爱的人（永邦）.mp3'),
+		duration: '04:36',
 	},
 	{
 		name: '偏偏喜欢你',
@@ -25,10 +31,10 @@ const MusicList = [
 		duration: '03:31',
 	},
 	{
-		name: '风继续吹',
-		singer: '张国荣',
-		url: require('./music/风继续吹（张国荣）.mp3'),
-		duration: '05:19',
+		name: '亲密爱人',
+		singer: '梅艳芳',
+		url: require('./music/亲密爱人（梅艳芳）.mp3'),
+		duration: '04:59',
 	},
 
 ]
